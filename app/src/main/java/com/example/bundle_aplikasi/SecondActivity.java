299ee80802ee18591d15819a68dbdc74cc1ac613
Habib Santoso <habib.santoso.ft18@mail.umy.ac.id>
2021-03-25 18:01:47 +0700

    penambahan tahap pengambilan data dari bundle yang dikirim dari Mainacitvity

--- app/src/main/java/com/example/bundle_aplikasi/SecondActivity.java
+++ app/src/main/java/com/example/bundle_aplikasi/SecondActivity.java
@@ -1,14 +1,27 @@
 package com.example.bundle_aplikasi;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
+    TextView chname, chpass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        chname = findViewById(R.id.tBunNama);
+        chpass = findViewById(R.id.tBunPass);
+
+        Bundle hbundata = getIntent().getExtras();
+
+        String hmail = hbundata.getString("email");
+        String hpass = hbundata.getString("passwords");
+
+        chname.setText(hmail);
+        chpass.setText(hpass);
     }
 }
